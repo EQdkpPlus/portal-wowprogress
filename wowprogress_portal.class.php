ef08c9f2ab4f5ad41e731598644713a1c10e529a
GodMod <GodMod@users.noreply.github.com>
2015-05-04 11:46:47 +0200

    Update wowprogress_portal.class.php

--- wowprogress_portal.class.php
+++ wowprogress_portal.class.php
@@ -29,7 +29,7 @@ class wowprogress_portal extends portal_generic {
 	protected static $path		= 'wowprogress';
 	protected static $data		= array(
 		'name'			=> 'wowprogress',
-		'version'		=> '0.3.2',
+		'version'		=> '0.3.3',
 		'author'		=> 'GodMod',
 		'contact'		=> EQDKP_PROJECT_URL,
 		'description'	=> 'Shows the WoW Guildprogress',
